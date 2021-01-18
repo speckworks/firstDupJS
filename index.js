59f72cfdefb59118720d85f8da7dc0e5cf4addab
John K. Speck <speckworks@gmail.com>
2021-01-18 00:15:42 +0000

    got all elements of Array in Object

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-a = [2,1,7,5,3]
+a = [2, 1, 7, 2, 5, 3, 2, 5, 1]
 
-const firstDuplicate = arr => {
+const firstNonDuplicate = arr => {
   const memory = {};
 
+  for(let j = 0; j < arr.length; j++){
+  memory[j] = arr[j];
+  };
+
+  return memory;
+
   for (let i = 0; i < arr.length; i++) {
-    if (memory[arr[i]] !== undefined) {
+    if (memory[arr[i]] == undefined) {
       return arr[i];
     } else {
       memory[arr[i]] = arr[i];
     }
   }
 
-  return "There are no duplicates."
+  return "There are no Non-duplicated numbers."
 };
 
-firstDuplicate(a);
+firstNonDuplicate(a);
