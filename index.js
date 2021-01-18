a60e2d2c5dbc06215dfd9963e8a05e1f25c3f73e
John K. Speck <speckworks@gmail.com>
2021-01-18 04:24:30 +0000

    durble durp

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 a = [2, 1, 7, 2, 5, 3, 2, 5, 1]
 
 const firstNonDuplicate = arr => {
-  const memory = {};
-
-  for(let j = 0; j < arr.length; j++){
-  memory[j] = arr[j];
-  };
-
-  return memory;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (memory[arr[i]] == undefined) {
-      return arr[i];
-    } else {
-      memory[arr[i]] = arr[i];
-    }
-  }
-
-  return "There are no Non-duplicated numbers."
-};
+    let findFirst = new Set(arr)
+    console.log(findFirst);
+    for (let i = 0; i < arr.length; i++){}
+    
+    
+ }
 
 firstNonDuplicate(a);
