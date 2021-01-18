dc167985fdf0a807c2a0270e84883281e1185bb2
John K. Speck <speckworks@gmail.com>
2021-01-18 17:57:14 +0000

    O(n) solution

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 a = [2, 1, 7, 2, 5, 3, 2, 5, 1]
 
 const firstNonDuplicate = arr => {
-    let findFirst = new Set(arr)
-    console.log(findFirst);
-    for (let i = 0; i < arr.length; i++){}
-    
-    
+     let freqCounter = [];
+
+    // The issue using Map data structure is during the retrival. As it does not gaurantee the keys will be retrived in the same order as they were inserted
+    // Hence, we use an array of frequency counter. But in this array keys are found using the ascii values of the character.
+
+    arr.forEach(ch => {
+        if (!freqCounter[ch])
+            freqCounter[ch] = 1;
+        else
+            freqCounter[ch]++;
+    });
+
+    // Observe this array. It's kinda Map only.
+    //console.log(freqCounter);
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        let ch = arr[i];
+        if (freqCounter[ch] == 1)
+            return ch;
  }
+}
 
 firstNonDuplicate(a);
