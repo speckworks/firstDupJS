809348f5e33f6746e1224dfddb2d1330b7c95e07
John K. Speck <speckworks@gmail.com>
2021-01-20 02:36:33 +0000

    Passed All the Tests Using ".every". Yeee Hawwww!

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-a = [2, 1, 7, 2, 5, 3, 2, 5, 1]
+//a = [2, 1, 7, 2, 5, 3, 2, 5, 1]
+a = "aabbccddee"
 
-const firstNonDuplicate = arr => {
-     let freqCounter = [];
-
-    // The issue using Map data structure is during the retrival. As it does not gaurantee the keys will be retrived in the same order as they were inserted
-    // Hence, we use an array of frequency counter. But in this array keys are found using the ascii values of the character.
-
+const firstNonDuplicate = s => {
+     let freqCounter = {};
+      let arr = s.split('');
+console.log(arr);
     arr.forEach(ch => {
         if (!freqCounter[ch])
-            freqCounter[ch] = 1;
+            freqCounter[ch] = 1; 
         else
             freqCounter[ch]++;
     });
 
-    // Observe this array. It's kinda Map only.
+//console.log(freqCounter);
     //console.log(freqCounter);
 
-    for (let i = 0; i < arr.length - 1; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let ch = arr[i];
         if (freqCounter[ch] == 1)
             return ch;
- }
+    }
+   
+ if(Object.keys(freqCounter).every(key => freqCounter[key] > 1)){return "_"};
+
+
 }
 
 firstNonDuplicate(a);
